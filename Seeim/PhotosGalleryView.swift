6ee31eb91e9793e9804670d8414189cbb2254c20
sypianBS <sypianbs@gmail.com>
2022-12-18 21:22:00 +0100

    add header

--- Seeim/PhotosGalleryView.swift
+++ Seeim/PhotosGalleryView.swift
@@ -17,16 +17,30 @@ struct PhotosGalleryView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(photoDataDownloadViewModel.photoModels) { model in
-                    GallerySingleImageView(urlString: model.downloadURL)
+        VStack(spacing: 0) {
+            Text("Seeim")
+                .font(.system(size: 36, weight: .semibold, design: .serif))
+                .foregroundColor(Color.init(red: 143/255, green: 0/255, blue: 255/255))
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 8)
+                .background {
+                    Color.yellow
+                        .opacity(0.6)
+                        .ignoresSafeArea()
                 }
-            }.padding(.horizontal, 32)
-        }.background {
-            Color.yellow
-                .opacity(0.3)
-                .ignoresSafeArea()
+            ScrollView {
+                Spacer()
+                    .frame(height: 8)
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(photoDataDownloadViewModel.photoModels) { model in
+                        GallerySingleImageView(urlString: model.downloadURL)
+                    }
+                }.padding(.horizontal, 32)
+            }.background {
+                Color.yellow
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+            }
         }
     }
 }
